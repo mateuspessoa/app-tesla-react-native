50c937cf6f97f38ff9e2ada5b679b1db55b4b1ab
Mateus de Oliveira Pessoa <mateuspessoadev@gmail.com>
2022-06-17 13:04:34 -0300

    Projeto Tesla finalizado

---
+++ components/CarItem/index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { View, Text, ImageBackground } from 'react-native';
+
+import StyledButton from '../StyledButton';
+import styles from './styles';
+
+const CarItem = (props) => {
+
+
+    const { name, tagline, taglineCTA, image } = props.car;
+
+
+    return(
+
+        <View style={styles.carContainer}>
+
+        <ImageBackground source={image} style={styles.image} />
+
+        <View style={styles.titles}>
+          <Text style={styles.title}>{name}</Text>
+          <Text style={styles.subtitle}>
+            {tagline} {' '}
+            <Text style={styles.taglineCTA}>{taglineCTA}</Text>
+          </Text>
+        </View>
+
+        <View style={styles.buttonsContainer}>
+
+          <StyledButton type="primary" content={"Customizar Modelo"} onPress={() => {console.warn("Pressionado primario")}}/>
+
+          <StyledButton type="secundary" content={"Consultar Disponibilidade"} onPress={() => {console.warn("Pressionado secundário")}}/>
+
+        </View>
+
+      </View>
+
+    );
+};
+
+export default CarItem;
---
+++ components/CarsList/cars.js
@@ -0,0 +1,20 @@
+export default [{
+  name: 'Model S',
+  tagline: 'A partir de $69,420',
+  image: require('../../assets/images/ModelS.jpeg'),
+}, {
+  name: 'Model 3',
+  tagline: 'Compre Online',
+  taglineCTA: 'Realizar Pedido',
+  image: require('../../assets/images/Model3.jpeg'),
+}, {
+  name: 'Model X',
+  tagline: 'Compre Online',
+  taglineCTA: 'Realizar Pedido',
+  image: require('../../assets/images/ModelX.jpeg'),
+}, {
+  name: 'Model Y',
+  tagline: 'Compre Online',
+  taglineCTA: 'Realizar Pedido',
+  image: require('../../assets/images/ModelY.jpeg'),
+}];
